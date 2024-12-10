385b752b2c344ef204e99b4d02f303b1426e739d
student <andrusondrfgvokivaN@gmail.com>
2024-12-10 08:51:39 +0700

    student

--- store/Admission.php
+++ store/Admission.php
@@ -19,23 +19,57 @@ $name_pro = $pdo->query("select name from product where id = '$id'")->fetch(PDO:
 <body>
 <style>
 
+    .container{
+        margin: 20px 30px;
+    }
    .card{
        display: flex;
        flex-direction: column;
-       justify-content: center;
        border-bottom: 2px black solid;
+       border: 1px black solid;
+       padding: 20px;
+       border-radius: 15px;
    }
    .cards{
        display: flex;
        gap: 60px;
    }
+   .nav a{
+       display: flex;
+       flex-direction: column;
+       justify-content: center;
+       align-items: center;
+       text-decoration: none;
+       color: gray;
+       margin-top: 20px;
+   }
+   .nav a:hover{
+       color: black;
+   }
+   h1{
+       text-align: center;
+       margin-bottom: 30px;
+   }
+   .nav{
+       margin-bottom: 100px;
+   }
+    .title{
+        font-size: 20pt;
+        text-align: center;
+        border-bottom: 1px black solid;
+        color: #474646;
+    }
 </style>
 <div class="container">
     <h1>Поступление</h1>
+    <div class="nav">
+        <a href="index.php">Назад</a>
+        <a href="Add_admission.php?id_product=<?=$id?>"> Добавить информацию о поступлении товара</a>
+    </div>
         <div class="cards">
             <?php foreach ($characteristics as $item):?>
                 <div class="card">
-                    <p>Название - <?=$name_pro['name']?></p>
+                    <p class="title"> <?=$name_pro['name']?></p>
                     <p>Дата поступления - <?=$item['date_time']?></p>
                     <p>количество - <?=$item['amount']?></p>
                     <a href="/handler/delete.php?id=<?=$item['id']?>">Удалить поступление</a>
@@ -45,8 +79,7 @@ $name_pro = $pdo->query("select name from product where id = '$id'")->fetch(PDO:
 
     </div>
 </div>
-<a href="index.php">Назад</a>
-<a href="Add_admission.php?id_product=<?=$id?>"> Добавить информацию о поступлении товара</a>
+
 
 </body>
 </html>
